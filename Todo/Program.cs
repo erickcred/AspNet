b07c9a286b1af155805ccf8fdf57ea382a9af367
Ercik Rick <erickcredjes@gmail.com>
2022-06-25 19:17:20 -0300

    Setando a aplicação para apresentar Views

--- Todo/Program.cs
+++ Todo/Program.cs
@@ -3,6 +3,7 @@ using Todo.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
